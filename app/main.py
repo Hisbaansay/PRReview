8256ebecc721ba26b88a5ef82fea8c9fd6bc6126
Hisbaansay <hisbaansayed7@gmail.com>
2025-09-18 11:41:16 +0200

    changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 import os  # TODO: remove unused import
 
 def greet(name):
-    print("Hello " + name)  # noqa: E999  # long line will trigger linter
-
-if __name__ == "__main__":
-    greet("World")
+    print("Hello " + name)  # noqa: E999
